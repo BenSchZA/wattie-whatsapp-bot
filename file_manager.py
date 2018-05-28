8a2d6a2d7c55f5cffba016e9f24d187ac0b2b073
BenSchZA <bscholtz.bds@gmail.com>
2018-05-28 18:10:04 +0200

    Update FileManager class to handle local db unique indexes

--- file_manager.py
+++ file_manager.py
@@ -3,16 +3,23 @@ import errno
 import urllib.request
 from urllib.parse import urlparse
 import time
-import datetime
+from datetime import datetime
+from datetime import timezone
 from pymongo import MongoClient
+import pymongo
+from pymongo.collection import Collection
+from pymongo.errors import WriteError
 import log_manager
+from user import User
+from firebase_manager import FirebaseManager
+import utils
 
 LOGS = 'logs'
 DOWNLOADS = 'downloads'
 
 
 class FileManager:
-    
+
     def __init__(self) -> None:
         super().__init__()
         self._initialize_directories()
@@ -22,8 +29,10 @@ class FileManager:
     def _initialize_database(self):
         self.client = MongoClient()
         self.db = self.client.file_manager
+        self.firebase = FirebaseManager()
 
-        self.downloads_collection = self.db.downloads_collection
+        self.downloads_collection: Collection = self.db.downloads_collection
+        self.downloads_collection.create_index([("uid", pymongo.DESCENDING), ("url", pymongo.DESCENDING)], unique=True)
 
     def _initialize_directories(self):
         self._create_directory(LOGS)
@@ -38,39 +47,49 @@ class FileManager:
             if e.errno != errno.EEXIST:
                 raise
 
-    def download_and_schedule_from_url(self, uid, url, subfolder, scheduled_millis):
-        # Parse url and create path for download
-        parsed = urlparse(url)
-        filename = os.path.basename(parsed.path)
-        directory = DOWNLOADS + '/' + subfolder
-        path = os.path.join(directory, filename)
+    def download_and_schedule(self):
+        firebase_scheduled = self.firebase.get_scheduled_deliveries_hour()
 
-        # Create directory structure and download file
-        self._create_directory(directory)
-        urllib.request.urlretrieve(url, path)
+        for user in firebase_scheduled:
+            # Parse url and create path for download
+            url = user.***REMOVED***_url
 
-        # Create database entry for logging and management
-        self._log_download(uid, url, path, scheduled_millis)
-        return path
+            parsed = urlparse(url)
+            filename = os.path.basename(parsed.path)
+            pwd = os.getcwd()
+            directory = pwd + DOWNLOADS + '/' + user.uid
+            path = os.path.join(directory, filename)
 
-    def _log_download(self, uid, url, path, scheduled_millis):
+            # Create directory structure and download file
+            self._create_directory(directory)
+            urllib.request.urlretrieve(url, path)
+
+            # Create database entry for logging and management
+            self._log_schedule(user, path)
+
+    def _log_schedule(self, user: User, path):
         # Create download_collection object for insertion into database
-        created = time.ctime()
         delivered = False
         download = {
-            'uid': uid,
-            'url': url,
+            'uid': user.uid,
+            'name': user.name,
+            'number': user.number,
+            'url': user.***REMOVED***_url,
             'path': path,
-            'scheduled_millis': scheduled_millis,
+            'scheduled_millis': user.get_user_scheduled_time_millis_utc(),
             'delivered': delivered,
-            'created_millis': created,
-            'created_date': datetime.datetime.utcnow()
+            'created_millis': utils.time_in_millis_utc(),
+            'created_date': datetime.utcnow()
         }
 
         # Insert object into downloads_collection and log database uid
-        download_id = self.downloads_collection.insert_one(download).inserted_id
-        self.logger.info('File downloaded & stored in database with ID ' + str(download_id))
-        return download_id
+        try:
+            download_id = self.downloads_collection.insert_one(download).inserted_id
+            self.logger.info('File downloaded & stored in database with ID ' + str(download_id))
+            return download_id
+        except WriteError as e:
+            self.logger.info('Entry exists for user %s & url %s' % (user.uid, user.***REMOVED***_url))
+            return None
 
     def _get_download_entry_for_uid(self, uid):
         self.downloads_collection.find_one({"uid": uid})
@@ -80,11 +99,20 @@ class FileManager:
 
     def _get_undelivered_downloads(self):
         return self.downloads_collection.find({"delivered": False,
-                                               "scheduled_millis": {"$lt": str(time.time())}
-                                               })
+                                               "scheduled_millis": {"$lt": utils.time_in_millis_utc()}})
 
-    def _get_scheduled_deliveries(self):
-        one_hour_in_seconds = 3600
-        self.downloads_collection.find({"scheduled_millis": {"$lt": str(time.time() + one_hour_in_seconds),
-                                                             "$gt": str(time.time())}
-                                        })
+    def get_scheduled_deliveries(self):
+        one_hour_in_millis = 3600000
+        return self.downloads_collection.find({
+            "delivered": False,
+            "scheduled_millis": {
+                "$lt": utils.time_in_millis_utc() + one_hour_in_millis,
+                "$gt": utils.time_in_millis_utc()
+            }})
+
+    def mark_delivered(self, uid):
+        self.downloads_collection.update_one(
+            {"uid": uid},
+            {"$set": {
+                "delivered": True
+            }})
