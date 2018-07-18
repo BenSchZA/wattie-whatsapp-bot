71b8ce4e796f72c299db2aa76043fb9d2dd9e5ea
BenSchZA <bscholtz.bds@gmail.com>
2018-07-18 16:08:44 +0200

    Refactor & New user processing

--- whatsapp_receive.py
+++ whatsapp_receive.py
@@ -3,6 +3,7 @@
 import time
 import datetime
 import dateutil.parser
+from difflib import SequenceMatcher
 
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
@@ -20,8 +21,11 @@ from pymongo.errors import WriteError
 
 from session_manager import SessionManager
 from message import Message
+import re
+import requests
 
 TIMEOUT = 30
+MESSAGE_LIMIT = 5
 
 binary = FirefoxBinary('/usr/bin/firefox-developer-edition')
 webdriver.DesiredCapabilities.FIREFOX["unexpectedAlertBehaviour"] = "accept"
@@ -52,13 +56,52 @@ class WhatsAppReceive:
         self.db = self.client.whatsapp_cli
 
         # MongoDB scales horizontally, so: single messages collection containing all messages from all conversations
-        #
         self.messages_collection: Collection = self.db.messages
-        # self.messages_collection.create_index([("uid", pymongo.DESCENDING)], unique=True)
+        self.messages_collection.create_index([("uid", pymongo.DESCENDING), ("timestamp", pymongo.DESCENDING),
+                                               ("content", pymongo.DESCENDING)], unique=True)
 
     def process_contacts(self):
         self._for_each_conversation(self._process_contact)
+        self.processed_contacts = list(map(lambda contact: self._clean_contact_number(contact), self.processed_contacts))
+        self.processed_contacts = list(filter(None.__ne__, self.processed_contacts))
+
+        for number in self.processed_contacts:
+            print('Number: %s _ %s' % (number, self.get_uid_from_number(number)))
         print(self.processed_contacts)
+
+    def _clean_contact_number(self, contact_number):
+        contact_number = contact_number.replace(' ', '')
+        if self._is_valid_numeric(contact_number):
+            return contact_number
+        else:
+            return None
+
+    def get_uid_from_number(self, contact_number):
+        data = {
+            'content-type': 'application/json',
+            'fullMobileNum': contact_number
+        }
+
+        headers = {
+            'Authorization': '56128eebf5a64cb6875b8d1c4789b216cf2331aa',
+            'Origin': 'https://my***REMOVED***.com'
+        }
+
+        req = requests.post("***REMOVED***:3000/user_uid_from_full_mobile_num", data=data, headers=headers)
+
+        if req.status_code == 200:
+            return req.json()['userUID']
+        else:
+            print(req.status_code, req.reason)
+            return None
+
+    def _is_valid_numeric(self, string):
+        for char in string:
+            if char.isdigit() or char == '+':
+                continue
+            else:
+                return False
+        return True
 
     def process_messages(self):
         self._for_each_conversation(self._process_conversation)
@@ -72,45 +115,24 @@ class WhatsAppReceive:
         print('Fetching conversations')
         conversations: [WebElement] = lambda: conversations_panel.find_elements_by_class_name('_2wP_Y')
 
-        # print('Processing conversations')
-        # conversations_copy = list(reversed(conversations())).copy()
-        # first_element = conversations_panel.find_element_by_xpath("(.//div[@class='_2wP_Y'])[1]")
-        # current_element = first_element
-        #
-        # index = 1
-        # while True:
-        #     print(current_element.text.replace('\n', ' '))
-        #     func(current_element)
-        #     try:
-        #         # current_element = current_element.find_element_by_xpath("./following-sibling::div[@class='_2wP_Y']")
-        #         current_element = conversations_panel.find_element_by_xpath("(.//div[@class='_2wP_Y'])[2]")
-        #     except NoSuchElementException:
-        #         break
-        #     self._scroll_into_view(current_element)
-
-        # conversations_copy = list(reversed(conversations())).copy()
-        #
-        # offset = conversations_copy[0].get_attribute('offsetTop')
-        #
-        # sorted_copy = sorted(conversations(), key=lambda el: el.get_attribute('offsetTop'))
-
         print('Processing conversations')
         while True:
-            def conversations_copy(): return sorted(conversations(), key=lambda el: self._get_element_y_position(el))
-            for conversation in conversations_copy():
-                func(conversation)
+            def sorted_conversations(): return sorted(conversations(), key=lambda el: self._get_element_y_position(el))
+            # conversations_copy = sorted_conversations().copy()
+            # print(list(map(lambda conv: '%s ~ %s' % (conv.id, conv.text), conversations_copy)))
+            for index in range(len(sorted_conversations())):
+                func(sorted_conversations()[index])
 
             scroll_progress = self._side_pane_scroll_top()
             scroll_max = self._side_pane_scroll_top_max()
             if scroll_progress == scroll_max:
                 break
 
-            last_processed_conversation = conversations_copy()[-1]
-            print(last_processed_conversation.id)
+            last_processed_conversation = sorted_conversations()[-1]
+            self._scroll_into_view(last_processed_conversation, True)
+            time.sleep(0.1)
 
-            self._scroll_into_view(last_processed_conversation)
             progress = round((scroll_progress/scroll_max)*100)
-            print('Scroll max: %s; Scroll top: %s' % (scroll_max, scroll_progress))
             print('Progress: %s' % progress)
 
     def _process_contact(self, conversation: WebElement):
@@ -123,23 +145,82 @@ class WhatsAppReceive:
         return contact_name_number
 
     def _process_conversation(self, conversation: WebElement):
-        name = None
+        uid = None
         try:
-            name = self.wait.until(lambda _: conversation.find_element_by_xpath(".//span[@class='_1wjpf']")) \
+            # Assuming the user is not saved as a contact, 'contact_id' will return the number
+            contact_id = self.wait.until(lambda _: conversation.find_element_by_xpath(".//span[@class='_1wjpf']")) \
                 .get_attribute('title')
-            last_message = self.wait.until(lambda _: conversation.find_element_by_xpath(".//span[@class='_2_LEW']")) \
-                .get_attribute('title')
-            print('Processing conversation %s: Name - %s ~ Last Message - %s' % (conversation.id, name, last_message))
-        except NoSuchElementException:
-            print('No such element')
 
-        if name in self.processed_conversation_ids:
+            contact_id = self._clean_contact_number(contact_id)
+            if not contact_id:
+                print('\nInvalid contact ID')
+                return False
+
+            # Try get uid from local database, otherwise perform network call,
+            # if this fails then user needs to be created first
+            uid = self.get_uid_from_number_db(contact_id)
+            if not uid:
+                uid = self.get_uid_from_number(contact_id)
+            if not uid:
+                print('\nUser needs to be created')
+
+            last_message_content = self.wait.until(lambda _: conversation.find_element_by_xpath(".//span[@class='_2_LEW']")) \
+                .get_attribute('title')
+
+            last_message = Message(uid=uid, timestamp=None, sender_name=None, sender_number=contact_id, content=last_message_content)
+            del last_message.timestamp
+            del last_message.sender_name
+
+            if self.messages_in_sync(last_message):
+                print('Messages in sync')
+                return True
+
+            print('\nProcessing conversation %s: ID - %s ~ Last Message - %s' % (conversation.id, contact_id, last_message_content))
+        except NoSuchElementException:
+            print('\nNo such element')
             return False
+
+        messages_panel = self.load_conversation_messages_panel(conversation)
+        self.previous_conversation_content = messages_panel
+
+        if uid:
+            messages = self._extract_and_save_messages(messages_panel)
+            print('Saving messages to database')
+            for message in messages:
+                message.uid = uid
+                self.save_message(message)
+                print('Message: %s' % message.__dict__)
+            return True
         else:
-            self.processed_conversation_ids.append(name)
+            username = self._process_new_user(messages_panel)
+            if username:
+                print('New user %s ~ %s' % (username, contact_id))
+            else:
+                print('No valid launch sequence for %s' % contact_id)
+            return False
+
+    def load_conversation_messages_panel(self, conversation):
+
+        self.wait.until(lambda _: conversation and conversation.is_displayed() and conversation.is_enabled())
+        while True:
+            try:
+                conversation.click()
+                break
+            except ElementClickInterceptedException:
+                time.sleep(0.1)
+                continue
 
         # If moving from active conversation, wait for content to refresh after click
-        conversation.click()
+        # while True:
+        #     self.wait.until(lambda _: conversation and conversation.is_displayed() and conversation.is_enabled())
+        #     conversation.click()
+        #     try:
+        #         conversation.click()
+        #         break
+        #     except ElementClickInterceptedException:
+        #         self._scroll_into_view(conversation, False)
+        #         continue
+
         if self.previous_conversation_content:
             self.wait.until(exp_c.staleness_of(self.previous_conversation_content))
 
@@ -149,28 +230,21 @@ class WhatsAppReceive:
 
         # Scroll through all messages until 100 messages are scraped, or we reach the top
         try:
-            while len(messages_panel.find_elements_by_class_name('vW7d1')) < 1:
+            while len(messages_panel.find_elements_by_class_name('vW7d1')) < MESSAGE_LIMIT:
                 try:
                     load_spinner = WebDriverWait(self.driver, 2) \
                         .until(lambda _: self.driver.find_element_by_xpath("//div[@class='_3dGYA']"))
-                    self._scroll_into_view(load_spinner)
-                except TimeoutException:
+                    self._scroll_into_view(load_spinner, True)
+                except (TimeoutException, StaleElementReferenceException):
                     break
                 self.wait.until(lambda _: not self._messages_are_loading())
         except NoSuchElementException:
             pass
 
-        self.previous_conversation_content = messages_panel
-        messages = self._extract_conversation_messages(messages_panel)
+        return messages_panel
 
-        print('Saving messages to database')
-        for message in messages:
-            self.save_message(message)
-
-        return True
-
-    def _scroll_into_view(self, web_element):
-        return self.driver.execute_script('return arguments[0].scrollIntoView(true);', web_element)
+    def _scroll_into_view(self, web_element, align_top: bool):
+        return self.driver.execute_script('return arguments[0].scrollIntoView(%s);' % 'true' if align_top else 'false', web_element)
 
     def _get_element_y_position(self, web_element):
         return self.driver.execute_script('return arguments[0].getBoundingClientRect().top;', web_element)
@@ -208,53 +282,98 @@ class WhatsAppReceive:
             pass
         return conversations_panel
 
-    def _extract_conversation_messages(self, messages_panel):
-        message_elements: [WebElement] = lambda: messages_panel.find_elements_by_class_name('vW7d1')
+    def _extract_and_save_messages(self, messages_panel):
         messages: [Message] = []
 
-        number_elements = len(message_elements())
-        print('Number: %s' % number_elements)
-        print('Copy: %s' % list(message_elements()))
+        def append_message(message): messages.append(message)
+        self._for_each_message(messages_panel, append_message)
 
+        return messages
+
+    def _process_new_user(self, messages_panel):
+        messages: [Message] = []
+        username = None
+
+        def append_message(msg): messages.append(msg)
+        self._for_each_message(messages_panel, append_message)
+
+        for message in messages:
+            # Split message content into words
+            word_list = message.content.split()
+            # Strip whitespace
+            word_list = [word.strip() for word in word_list]
+
+            # Find launch words
+            launch_words = list(filter(lambda word: self.similar('launch', word.lower(), 0.7)
+                                or self.similar('start', word.lower(), 0.7), word_list))
+
+            if not launch_words or not message.content.strip().startswith(launch_words[0]):
+                continue
+
+            # Remove launch words
+            word_list = [word for word in word_list if word not in launch_words]
+
+            # Remaining words should be name and surname
+            if not word_list:
+                continue
+
+            name, *surname = word_list
+            username = " ".join(word_list)
+            # Remove non-alpha characters
+            username = "".join([c for c in username if c.isalpha() or c.isspace()])
+
+            if username:
+                return username
+
+        return username
+
+    def similar(self, a, b, threshold):
+        return SequenceMatcher(None, a, b).ratio() >= threshold
+
+    def _for_each_message(self, messages_panel, func):
+        message_elements: [WebElement] = lambda: messages_panel \
+            .find_elements_by_xpath(".//div[@class='vW7d1'][position() <= %d]" % MESSAGE_LIMIT)
+
+        number_elements = len(message_elements())
         for index in range(number_elements):
             try:
-                details_el: WebElement = message_elements()[index].find_element_by_xpath(".//div[@class='Tkt2p']/div[1]")
+                details_el: WebElement = message_elements()[index] \
+                    .find_element_by_xpath(".//div[@class='Tkt2p']/div[1]")
             except NoSuchElementException:
                 try:
-                    details_el: WebElement = message_elements()[index].find_element_by_xpath(".//div[@class='Tkt2p']/div[2]")
+                    details_el: WebElement = message_elements()[index] \
+                        .find_element_by_xpath(".//div[@class='Tkt2p']/div[2]")
                 except NoSuchElementException:
-                    print('No details')
                     continue
             details = details_el.get_attribute('data-pre-plain-text')
 
             if details:
-                print('Details: %s' % details)
                 time_string: str = details[details.find('[')+1:details.find(']')]
-                sender_name: str = details.replace('[%s]' % time_string, '', 1).strip().replace(':', '', 1)
+                sender_id: str = details.replace('[%s]' % time_string, '', 1).strip().replace(':', '', 1)
             else:
-                print('No details')
                 continue
 
-            content: str = self.wait.until(lambda x: message_elements()[index].find_element_by_xpath(
-                "//span[@class='selectable-text invisible-space copyable-text']")).text
+            # content: str = self.wait.until(lambda x: message_elements()[index].find_element_by_xpath(
+            #     ".//span[@class='selectable-text invisible-space copyable-text']")).text
+            try:
+                content: str = message_elements()[index].find_element_by_xpath(
+                    ".//span[@class='selectable-text invisible-space copyable-text']").text
+            except NoSuchElementException:
+                continue
 
-            message = self.create_message(sender_name, time_string, sender_name, content)
-            print('Message: %s' % message)
-            messages.append(message)
+            message = self.create_message('', time_string=time_string, sender_name=None,
+                                          sender_number=sender_id, content=content)
+            func(message)
 
-        return messages
-
-    def create_message(self, uid, time_string, sender_name, content):
+    def create_message(self, uid, time_string, sender_name, sender_number, content):
         # Time string format: [18:44, 7/8/2018]
         # See http://strftime.org/
-        # try:
-        #     timestamp = datetime.datetime.strptime(time_string, "%H:%M, %m/%d/%Y").replace(tzinfo=datetime.timezone.utc)
-        # except ValueError:
-        #
+        # Timestamp can change, so using dateutil instead
+        # timestamp = datetime.datetime.strptime(time_string, "%H:%M, %m/%d/%Y").replace(tzinfo=datetime.timezone.utc)
 
         timestamp = dateutil.parser.parse(time_string).replace(tzinfo=datetime.timezone.utc)
+        msg = Message(uid, timestamp, sender_name, sender_number, content)
 
-        msg = Message(uid, timestamp, sender_name, content)
         return msg
 
     def save_message(self, msg: Message):
@@ -274,9 +393,28 @@ class WhatsAppReceive:
         print('Deleting message from database')
         self.messages_collection.delete_many({"_id": msg_id})
 
+    def purge_all_messages(self):
+        print('Deleting all messages from database')
+        self.messages_collection.delete_many({})
+
+    def get_uid_from_number_db(self, contact_number):
+        message = self.messages_collection.find_one({'sender_number': contact_number})
+        if message:
+            return message['uid']
+        else:
+            return None
+
+    def messages_in_sync(self, last_message: Message):
+        last_message_dict = last_message.__dict__
+        if self.messages_collection.find_one(last_message_dict):
+            return True
+        else:
+            return False
+
 
 if __name__ == '__main__':
     print('Procesing WhatsApp conversations')
     whatsapp_receive = WhatsAppReceive()
+    # print(whatsapp_receive.get_uid_from_contact_number('+27763381243'))
     # whatsapp_receive.process_contacts()
     whatsapp_receive.process_messages()
